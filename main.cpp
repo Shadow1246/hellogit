5dee7df4cf9f18b89fadfd0c33b2a4956876c1ef
Shadow1246 <Shadow1246@users.noreply.github.com>
2018-01-31 12:06:28 -0400

    Added header file

--- main.cpp
+++ main.cpp
@@ -8,4 +8,16 @@ int main()
 Fruit ob;
 Fruit ob2={700,Orange,1.00}
 
-vecto
+vector <Fruit> f;
+
+	f.push_back(ob);
+	f.push_back(ob2);
+	f.push_back({250, Banans,0.75});
+	f.push_back({1000,Apples,2.00});
+
+
+for (auto x:f)
+	{
+		cout << x.getNewamount() << x.getNewname() << x.getNewprice() <<endl;
+	}
+}
