e872e8dc0d1c249f0f8428eaf34fbcc75f9fba05
Shadow1246 <uShadow1246users.noreply.github.com>
2018-02-04 14:28:43 -0400

    Added header file

--- main.cpp
+++ main.cpp
@@ -8,19 +8,16 @@ using namespace std ;
 int main()
 {
 
-int Amount;
-string Name;
-float Price;
 
 Fruit ob;
-Fruit ob2={700,Orange,1.00};
+Fruit ob2=({700,Orange,1.00});
 
 vector <Fruit> f;
 
 	f.push_back(ob);
 	f.push_back(ob2);
-	f.push_back{250, Bananas,0.75};
-	f.push_back{1000,Apples,2.00};
+	f.push_back({250, Bananas,0.75});
+	f.push_back({1000,Apples,2.00});
 
 
 for (auto x:f)
