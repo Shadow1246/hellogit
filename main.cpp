2d4e2f45ca02f4f73e14696fb6105846696f6e63
Shadow1246 <uShadow1246users.noreply.github.com>
2018-02-04 14:30:51 -0400

    Added header file

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ int main()
 
 
 Fruit ob;
-Fruit ob2=({700,Orange,1.00});
+Fruit ob2=({700,Name,1.00});
 
 vector <Fruit> f;
 
 	f.push_back(ob);
 	f.push_back(ob2);
-	f.push_back({250, Bananas,0.75});
-	f.push_back({1000,Apples,2.00});
+	f.push_back({250, Name,0.75});
+	f.push_back({1000,Name,2.00});
 
 
 for (auto x:f)
