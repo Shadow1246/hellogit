3160cb2514d021a1c67d1db0e590d81148197285
Shadow1246 <Shadow1246@users.noreply.github.com>
2018-02-07 10:51:41 -0400

    Added main file

--- main.cpp
+++ main.cpp
@@ -10,19 +10,19 @@ int main()
 
 
 Fruit ob;
-
+Fruit ob2={700,"Orange",1.00};
 
 vector <Fruit> f;
 
 	f.push_back(ob);
-	f.push_back(ob);
-	f.push_back(ob);
-	f.push_back(ob);
+	f.push_back(ob2);
+	f.push_back({250,"Bananas",0.75});
+	f.push_back({1000,"Apples",2.00});
 
 
 for (auto x:f)
 	{
-		cout << x.getAmount() << x.getName() << x.getPrice() <<endl;
+		cout << x.getAmount()<<' ' << x.getName()<<' ' << x.getPrice() <<endl;
 	}
 
 	return 0;
